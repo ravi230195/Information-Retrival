d023792ad326477d3559fc66d890c0f1fe3de255
Ubuntu <ubuntu@ip-172-31-21-138.us-east-2.compute.internal>
2019-11-29 06:12:41 +0000

    initial

---
+++ src/Components/Header.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+function Header()
+{
+    return (
+        <div>
+
+                 
+          <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous"></link>
+          <meta name="viewport" content="width=device-width, initial-scale=1"/>
+          <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"></link>
+          <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+          <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" ></script>
+          <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" ></script>
+          <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+          <script src="https://unpkg.com/react@16.7.0-alpha.0/umd/react.development.js"></script>
+          <script src="https://unpkg.com/react-dom@16.7.0-alpha.0/umd/react-dom.development.js"></script>
+          <script src="https://developers.google.com/chart/interactive/docs/basic_load_libs#load-settings"> </script>
+        
+
+        </div>
+    )
+}
+
+export default Header;
---
+++ src/Components/Nav-bar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Navbar()
+{
+    return (
+        <div>
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div ><h3>QWERTY </h3></div>
+             </nav>
+    </div>
+    )
+}
+
+export default Navbar;
